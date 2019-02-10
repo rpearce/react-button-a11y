630bbb0ab07883018827f23dff9544bd0bc9823f
Robert Pearce <me@robertwpearce.com>
2019-02-10 16:57:54 +1300

    Updated example to use hooks

--- docs/app.js
+++ docs/app.js
@@ -11,55 +11,48 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+var App = function App() {
+  var _useState = (0, _react.useState)(false),
+      _useState2 = _slicedToArray(_useState, 2),
+      isBtn1Pressed = _useState2[0],
+      pressBtn1 = _useState2[1];
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+  var _useState3 = (0, _react.useState)(false),
+      _useState4 = _slicedToArray(_useState3, 2),
+      isBtn2Pressed = _useState4[0],
+      pressBtn2 = _useState4[1];
 
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-var App =
-/*#__PURE__*/
-function (_Component) {
-  _inherits(App, _Component);
-
-  function App() {
-    _classCallCheck(this, App);
-
-    return _possibleConstructorReturn(this, _getPrototypeOf(App).apply(this, arguments));
-  }
-
-  _createClass(App, [{
-    key: "render",
-    value: function render() {
-      return _react.default.createElement("div", null, _react.default.createElement("section", null, _react.default.createElement("h2", null, "Button with children"), _react.default.createElement(_.default, {
-        className: "btn--words",
-        onClick: function onClick() {
-          return window.alert('pressed first one');
-        }
-      }, "This does something")), _react.default.createElement("section", null, _react.default.createElement("h2", null, "Button without children"), _react.default.createElement(_.default, {
-        "aria-label": "This does something",
-        className: "btn--noWords",
-        onClick: function onClick() {
-          return window.alert('pressed second one');
-        }
-      })));
-    }
-  }]);
-
-  return App;
-}(_react.Component);
+  var handleBtn1Click = (0, _react.useCallback)(function () {
+    return pressBtn1(function (x) {
+      return !x;
+    });
+  }, [isBtn1Pressed]);
+  var handleBtn2Click = (0, _react.useCallback)(function () {
+    return pressBtn2(function (x) {
+      return !x;
+    });
+  }, [isBtn2Pressed]);
+  var btn1Label = isBtn1Pressed ? 'Pressed' : 'Press me';
+  var btn1Class = 'btn--words'.concat(isBtn1Pressed ? ' btn--pressed' : '');
+  var btn2Label = isBtn2Pressed ? 'Pressed' : 'Press me';
+  var btn2Class = 'btn--noWords'.concat(isBtn2Pressed ? ' btn--pressed' : '');
+  return _react.default.createElement("div", null, _react.default.createElement("section", null, _react.default.createElement("h2", null, "Button with children"), _react.default.createElement(_.default, {
+    className: btn1Class,
+    onClick: handleBtn1Click
+  }, btn1Label)), _react.default.createElement("section", null, _react.default.createElement("h2", null, "Button without children"), _react.default.createElement(_.default, {
+    "aria-label": btn2Label,
+    className: btn2Class,
+    onClick: handleBtn2Click
+  })));
+};
 
 var container = document.querySelector('[data-app]');
 
